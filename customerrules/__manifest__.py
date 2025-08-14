624b2adef3468b14ab8edea8876d26f0858a07ca
sarath-ksa <saraathkumar21@gmail.com>
2025-08-14 15:57:57 +0530

    add the module of customer_rules

---
+++ customerrules/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Customer Rules',
+    'version' : '18.0.0.1',
+    'author' : 'Digital',
+    'category' :'Customer',
+    'license' : 'LGPL-3',
+    'depends' : [],
+    'summary' : 'Set rules to customer',
+    'data' : [
+        'views/customerrule.xml',
+    ],
+    'installable' :True,
+    'application' :True,
+}
---
+++ customerrules/models/customerrules.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class CustomerTemplate(models.Model):
+    _inherit = 'res.partner'
+
+    # customercode=fields.Char(string='Customercode')
+
+
+    @api.onchange('vat')
+    def _checkvatid(self):
+        for rec in self:
+            if rec.vat:
+                exiting_vat=self.env['res.partner'].search([('vat','=',rec.vat)])
+                if exiting_vat:
+                   raise ValidationError("VAT ID already exisit")
